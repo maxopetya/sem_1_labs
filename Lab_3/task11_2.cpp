cd085b684a3c1a38ad736aab903ea85f71809397
maxopetya <maxopetya@yandex.ru>
2025-09-30 11:33:10 +0300

    test

--- Lab_3/task11_2.cpp
+++ Lab_3/task11_2.cpp
@@ -4,9 +4,9 @@
 int main() {
     std::string s = "Can you can a can as a canner can can a can?";
     std::string bylo = "can";
-    std::string stalo = "new_word";
+    std::string stalo = " new_word ";
     size_t pos = 0;
-    while ((pos = s.find("can", pos)) != std::string::npos) {
+    while ((pos = s.find(" can ", pos)) != std::string::npos) {
         s.replace(pos, 3, stalo);
         pos += 8;
     }
